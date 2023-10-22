d667c9310cf0a64b672e86f4d32ea88e0218d5e5
Vlad Sushchenko <71527547+6Glow@users.noreply.github.com>
2023-10-22 22:19:53 +0300

    three routers have been added
    three routers have been added

---
+++ controllers/users.js
@@ -0,0 +1,15 @@
+const login = async (req, res) => {
+  res.send('login');
+}
+const register = async (req, res) => {
+  res.send('login');
+}
+const current = async (req, res) => {
+  res.send('login');
+}
+
+module.exports = {
+  login,
+  register,
+  current,
+}
